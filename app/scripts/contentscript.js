ef28008dbf8bd2667e8435abf1dfa16593c4397a
Paul Leclercq <paul.leclercq@tabmo.io>
2015-10-11 12:12:55 +0200

    feat(twitter): remove t.co links for real ones

---
+++ app/scripts/contentscript.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//Twitter
+var allShortLinks = document.querySelectorAll('.js-display-url');
+for (var i = 0; i < allShortLinks.length; i++) {
+	var parentNode = allShortLinks[i].parentNode;
+	var fullURL = parentNode.getAttribute('data-expanded-url');
+	parentNode.setAttribute('href', fullURL);
+	parentNode.innerHTML = fullURL;
+}
+//# sourceMappingURL=contentscript.js.map
