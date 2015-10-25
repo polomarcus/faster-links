f11e116bf4015190f12259d883dac80a61b9e0b2
Paul Leclercq <paul.leclercq@tabmo.io>
2015-10-25 12:56:21 +0100

    test(twitter): test profile link #1

--- test/spec/test.js
+++ test/spec/test.js
@@ -11,6 +11,16 @@
       assert.equal(removeProtocol(url), 'fake.it');
     });
 
+    it('should replace t.co link with real profile URL', function () {
+     var profileURL = document.querySelector('.ProfileHeaderCard-urlText a');
+     assert.equal(profileURL.getAttribute('href'), 'http://t.co/jJkLxp36yI');
+
+     toCoBioURL()
+
+     profileURL = document.querySelector('.ProfileHeaderCard-urlText a');
+     assert.equal(profileURL.getAttribute('href'), 'http://www.wake-me-up.co');
+    });
+
     it('should remove every t.co URLs with their real URLs', function () {
       var tweet = document.querySelector('#testExtension');
       assert.equal(tweet.getAttribute('href'), 'http://t.co/fdmQ6yXo8G');
